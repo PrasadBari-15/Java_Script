9b47a4a798bbd97e9fa0e0839f00779dec87f90b
Prasad Bari <140627442+PrasadBari-15@users.noreply.github.com>
2024-10-20 15:01:15 +0000

    cointainer config done

---
+++ Basics/01_Variables.js
@@ -0,0 +1,20 @@
+console.log("**Variables**")
+const Account_id="1234";
+let AccountUserName="Prasad"
+var AccountType="Current";
+
+console.log(AccountUserName)
+console.log(Account_id)
+console.log(AccountType)
+
+//Account_id=2345
+
+AccountUserName="Prasad Bari"
+AccountType="Savings"
+console.log(AccountUserName)
+console.log(AccountType)
+
+let Balance;
+console.log(Balance)
+
+console.table([Account_id,AccountUserName,AccountType,Balance])
