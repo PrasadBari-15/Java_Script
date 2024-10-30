d2bc5eac8aa40dd290666010ff910d5352906f05
Prasad Bari <140627442+PrasadBari-15@users.noreply.github.com>
2024-10-30 15:45:57 +0000

    container config done

---
+++ Basics/02_DataTypes.js
@@ -0,0 +1,13 @@
+console.log(3+3)
+//number => 2 power 53
+//bigint
+//string => ""
+//boolean =>true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
