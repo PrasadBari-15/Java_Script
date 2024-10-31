18b05f0f4e999717da9b43867906b50781ab7a94
Prasad Bari <140627442+PrasadBari-15@users.noreply.github.com>
2024-10-31 12:39:10 +0000

    Comparision

--- Basics/04_Operators.js
+++ Basics/04_Operators.js
@@ -8,3 +8,9 @@ console.log(a++) // o/p => 1
 console.log(a) //o/p => 2
 let b=9
 console.log(++b) //o/p => 10
+
+console.log("2"+"6")
+console.log(1+2)
+console.log("4"+1)
+console.log("4"+3+9)
+console.log(2+3+"4")
---
+++ Basics/05_Comparision.js
@@ -0,0 +1,10 @@
+console.log("*****Comparision*****")
+
+console.log(2>3)
+console.log(1>=2)
+console.log(1>=1)
+
+console.log(null >= 0 )
+console.log(null == 0)
+
+console.log("4">3)
