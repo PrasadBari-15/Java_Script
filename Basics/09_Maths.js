8fc5ed0918094cc02e3910d8588824ca771c6df7
Prasad Bari <140627442+PrasadBari-15@users.noreply.github.com>
2024-11-03 18:23:08 +0000

    Maths&num

---
+++ Basics/09_Maths.js
@@ -0,0 +1,29 @@
+let score =400
+console.log(score)
+
+let balance =new Number(100)
+console.log(balance)
+
+console.log(balance.toFixed(2))
+let number =90.77
+console.log(number.toPrecision(3))
+
+//==================Maths==================
+console.log(Math)
+console.log(Math.abs(-4)) 
+console.log(Math.round(4.6))// round figure
+console.log(Math.ceil(4.3));
+console.log(Math.floor(8.2));
+console.log(Math.sqrt(9))
+console.log(Math.max(4,9.8))
+console.log(Math.min(4,3,9));
+
+
+console.log("Random ....")
+console.log(Math.random());
+console.log((Math.random()*10)+1)
+console.log(Math.floor(Math.random()*10)+1)
+
+let min=10
+let max=15
+console.log(Math.floor(Math.random()*(max-min+1))+min)
