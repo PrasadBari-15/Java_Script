9defadb16bf172059a38ba4ea433648ad8393ed5
Prasad Bari <140627442+PrasadBari-15@users.noreply.github.com>
2024-11-05 17:44:43 +0000

    json

---
+++ Basics02/05ObjectswithJson.js
@@ -0,0 +1,25 @@
+const course ={
+    coursename : "C++",
+    courseduration: "4 months",
+    coursefees : "300"
+}
+//course.coursename
+
+const {coursefees : fees} =course
+console.log(coursefees)
+console.log(fees)
+
+
+//Json
+//  const pr ={
+//     "name": "Prasad",
+//     "Roll_no": "30" ,
+//     "Branch" : "CSE" 
+//  }
+
+//in array form
+[
+    {},
+    {},
+    {}
+]
