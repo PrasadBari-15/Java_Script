28683d59c53557c5d27875cfd6acc07a324f1175
Prasad Bari <140627442+PrasadBari-15@users.noreply.github.com>
2024-11-09 18:30:42 +0000

    iifi

---
+++ Basics03/iifi.js
@@ -0,0 +1,12 @@
+// Imediately Invoke function
+
+(function chai(){
+    //name iifi
+    console.log(`DB connected`);
+    
+}) ();
+
+((name)=>{
+    console.log(`DB Connected ${name}`);
+    
+})('Prasad');
